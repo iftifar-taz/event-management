7270cdb9785914554e634bd84dfc2b39d1d07d8f
Iftifar Taz <iftifar.taz@northsouth.edu>
2024-12-22 04:53:43 +0600

    password match validation

--- frontend/src/lib/validations.ts
+++ frontend/src/lib/validations.ts
@@ -10,14 +10,19 @@ export const loginSchema = z.object({
   password: requiredString.min(1, { message: "Field is required" }),
 });
 
-export const registerSchema = z.object({
-  name: requiredString.min(1, { message: "Field is required" }),
-  email: requiredString
-    .email({ message: "Invalid Email" })
-    .min(1, { message: "Field is required" }),
-  password: requiredString.min(1, { message: "Field is required" }),
-  confirmPassword: requiredString.min(1, { message: "Field is required" }),
-});
+export const registerSchema = z
+  .object({
+    name: requiredString.min(1, { message: "Field is required" }),
+    email: requiredString
+      .email({ message: "Invalid Email" })
+      .min(1, { message: "Field is required" }),
+    password: requiredString.min(1, { message: "Field is required" }),
+    confirmPassword: requiredString.min(1, { message: "Field is required" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password do not match",
+    path: ["confirmPassword"],
+  });
 
 export const forgotPasswordSchema = z.object({
   email: requiredString
@@ -25,13 +30,23 @@ export const forgotPasswordSchema = z.object({
     .min(1, { message: "Field is required" }),
 });
 
-export const resetPasswordSchema = z.object({
-  password: requiredString.min(1, { message: "Field is required" }),
-  confirmPassword: requiredString.min(1, { message: "Field is required" }),
-});
+export const resetPasswordSchema = z
+  .object({
+    password: requiredString.min(1, { message: "Field is required" }),
+    confirmPassword: requiredString.min(1, { message: "Field is required" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password do not match",
+    path: ["confirmPassword"],
+  });
 
-export const changePasswordSchema = z.object({
-  currentPassword: requiredString.min(1, { message: "Field is required" }),
-  newPassword: requiredString.min(1, { message: "Field is required" }),
-  confirmNewPassword: requiredString.min(1, { message: "Field is required" }),
-});
+export const changePasswordSchema = z
+  .object({
+    currentPassword: requiredString.min(1, { message: "Field is required" }),
+    newPassword: requiredString.min(1, { message: "Field is required" }),
+    confirmNewPassword: requiredString.min(1, { message: "Field is required" }),
+  })
+  .refine((data) => data.newPassword === data.confirmNewPassword, {
+    message: "Password do not match",
+    path: ["confirmNewPassword"],
+  });
